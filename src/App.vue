103225ec428cc3dcfbd2937942f0b149382ca0f8
danielovic1358 <danielstroh283@gmail.com>
2026-06-12 15:36:26 +0200

    feat: Ändern einer Variable

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,11 @@ import axios from 'axios';
 
 const city = ref(<string>"") 
 
-const wjson = ref(<any>{})
+const weatherData = ref(<any>{})
 
 const handleSubmit = async() => {
   let response = await axios.get("http://api.weatherapi.com/v1/forecast.json?key=ba1c13e7efa44e93af174636260906&q=" + city.value + "&days=7&aqi=yes&alerts=yes&lang=de")
-  wjson.value = response.data
+  weatherData.value = response.data
 }
 
 </script>
@@ -19,8 +19,8 @@ const handleSubmit = async() => {
     <h2>Wetterinformationen für {{ city }}:</h2>
     <input v-model="city" type="text">
     <button type="submit" v-on:click="handleSubmit">Absenden</button>
-  <div v-if="wjson.forecast">
-    <div v-for="value in wjson.forecast.forecastday">
+  <div v-if="weatherData.forecast">
+    <div v-for="value in weatherData.forecast.forecastday">
       <h3>{{ value.date_epoch}}</h3>
       <p>Maximal erreichbare Temperatur: {{ value.day.maxtemp_c }}</p>
       <p>Gemeldete Wetterlage: {{ value.day.condition.text}}</p>
